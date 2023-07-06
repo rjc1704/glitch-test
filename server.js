aed42086df1c74f9a83f2eb75d053601987ac34f
Jecheon Ryu <rjc1704@gmail.com>
2023-07-06 12:07:06 +0900

    midware

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const cors = require("cors");
-// const middlewares = jsonServer.defaults();
+// const cors = require("cors");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
 
-// server.use(middlewares);
+server.use(middlewares);
 server.use(router);
-server.use(cors());
+// server.use(cors());
 
 server.listen(port, () => {
   console.log(`${port}에서 서버가 열렸습니다.`);
